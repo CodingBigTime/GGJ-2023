8ab98a55392bca2448a5423d13d9789f9bf8fa20
itrajkov <ivche1337@outlook.com>
2023-02-04 00:21:18 +0100

    Update get_joystick_distance() to return an int.

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name Player
 
 var current_point: Point = null
+var CONTROLLER_DEADZONE = 0.05
 
 func _physics_process(delta):
 	var left_stick_angle = get_joystick_direction(JOY_ANALOG_LX, JOY_ANALOG_LY)
@@ -9,6 +10,11 @@ func _physics_process(delta):
 
 	var left_stick_distance = get_joystick_distance(JOY_ANALOG_LX, JOY_ANALOG_LY)
 	var right_stick_distance = get_joystick_distance(JOY_ANALOG_RX, JOY_ANALOG_RY)
+
+	if(abs(right_stick_distance) > CONTROLLER_DEADZONE):
+		print(right_stick_distance)
+	if(abs(left_stick_distance) > CONTROLLER_DEADZONE):
+		print(left_stick_distance)
 
 func set_current_point(point: Point):
 	current_point = point
@@ -19,4 +25,4 @@ func get_joystick_direction(x, y):
 	return 2*PI - (stick_angle + 2 * PI if stick_angle < 0 else stick_angle)
 
 func get_joystick_distance(x, y):
-	return Vector2(Input.get_joy_axis(0, x), Input.get_joy_axis(0, y))
+	return  min(sqrt(pow(Input.get_joy_axis(0, x),2) + pow(Input.get_joy_axis(0, y), 2)), 1)
