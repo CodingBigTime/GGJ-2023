8707c74644475f15b253405ab4af8c4709ec32e3
itrajkov <ivche1337@outlook.com>
2023-02-03 23:45:46 +0100

    Add Y to joystick functions.

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -4,19 +4,19 @@ class_name Player
 var current_point: Point = null
 
 func _physics_process(delta):
-	var left_stick_angle = get_joystick_direction(JOY_ANALOG_LX)
-	var right_stick_angle = get_joystick_direction(JOY_ANALOG_RX)
+	var left_stick_angle = get_joystick_direction(JOY_ANALOG_LX, JOY_ANALOG_LY)
+	var right_stick_angle = get_joystick_direction(JOY_ANALOG_RX, JOY_ANALOG_RY)
 
-	var left_stick_distance = get_joystick_distance(JOY_ANALOG_LX)
-	var right_stick_distance = get_joystick_distance(JOY_ANALOG_RX)
+	var left_stick_distance = get_joystick_distance(JOY_ANALOG_LX, JOY_ANALOG_LY)
+	var right_stick_distance = get_joystick_distance(JOY_ANALOG_RX, JOY_ANALOG_RY)
 
 func set_current_point(point: Point):
 	current_point = point
 	position = point.position
 
-func get_joystick_direction(stick: int):
-	var stick_angle = Vector2(Input.get_joy_axis(0, stick), Input.get_joy_axis(0, stick)).angle()
+func get_joystick_direction(x, y):
+	var stick_angle = Vector2(Input.get_joy_axis(0, x), Input.get_joy_axis(0, y)).angle()
 	return 2*PI - (stick_angle + 2 * PI if stick_angle < 0 else stick_angle)
 
-func get_joystick_distance(stick: int):
-	return Vector2(Input.get_joy_axis(0, stick), Input.get_joy_axis(0, stick))
+func get_joystick_distance(x, y):
+	return Vector2(Input.get_joy_axis(0, x), Input.get_joy_axis(0, y))
