d6ebf2aa7204f7bf1635a3a823699c461ae3371a
LordDeatHunter <desuhansa@gmail.com>
2023-02-04 15:08:45 +0100

    Start node movement logic

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -3,6 +3,9 @@ class_name Player
 
 var current_point: Point = null
 var CONTROLLER_DEADZONE = 0.05
+var MOVE_DELAY = 0.25
+var MOVEMENT_LIMIT = PI/2
+var movement_delta = 1
 
 var points = 0
 var score_display = Label.new()
@@ -20,8 +23,22 @@ func _physics_process(delta):
 	var left_stick_angle = Utils.get_joystick_direction(JOY_ANALOG_LX, JOY_ANALOG_LY)
 	var right_stick_angle = Utils.get_joystick_direction(JOY_ANALOG_RX, JOY_ANALOG_RY)
 
-	var left_stick_distance = Utils.get_joystick_distance(JOY_ANALOG_LX, JOY_ANALOG_LY)
-	var right_stick_distance = Utils.get_joystick_distance(JOY_ANALOG_RX, JOY_ANALOG_RY)
+	movement_delta += delta
+
+	if(abs(right_stick_distance) > CONTROLLER_DEADZONE):
+		var connected_points = current_point.get_connector_points()
+		if connected_points.size() > 0 and movement_delta >= MOVE_DELAY:
+			movement_delta = 0
+			var closest_point = null
+			var min_angle = 1000
+			for point in connected_points:
+				var current_angle = abs(abs_min_angle(point.position.angle_to_point(current_point.position)) - abs_min_angle(right_stick_angle))
+				if current_angle < min_angle && abs(current_angle) < MOVEMENT_LIMIT:
+					closest_point = point
+					min_angle = current_angle
+			if (closest_point != null):
+				print(min_angle)
+				set_current_point(closest_point)
 
 	preview_point.update_position(
 		left_stick_angle,
@@ -45,10 +62,15 @@ func _physics_process(delta):
 
 	if (Input.is_action_just_pressed("remove_node")):
 		current_point.get_connector_points()[0].remove_edges()
-	
+
 	update()
 	score_display.set_text("Score: " + str(points))
 
+
+func min_angle(angle):
+	return min(2*PI-angle, angle)
+func abs_min_angle(angle):
+	return abs(min_angle(angle))
 
 func _draw():
 	if (preview_point.state != PreviewPoint.State.HIDDEN):
