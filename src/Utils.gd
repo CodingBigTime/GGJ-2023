ebdde1c8785010e7ff7e7d7e0730a13762a04727
Martin Popovski <martinkozle@yahoo.com>
2023-02-04 19:11:00 +0100

    Improve invalid and snap placement

--- src/Utils.gd
+++ src/Utils.gd
@@ -16,3 +16,22 @@ static func get_abs_angle_difference(a, b):
 	elif diff < -PI:
 		diff += 2 * PI
 	return abs(diff)
+
+static func are_segments_intersecting(start1, end1, start2, end2):
+	var d = (end1.x - start1.x) * (end2.y - start2.y) - (end1.y - start1.y) * (end2.x - start2.x)
+	if d == 0:
+		return false
+	var u = ((start2.x - start1.x) * (end2.y - start2.y) - (start2.y - start1.y) * (end2.x - start2.x)) / d
+	var v = ((start2.x - start1.x) * (end1.y - start1.y) - (start2.y - start1.y) * (end1.x - start1.x)) / d
+	return u > 0 and u < 1 and v > 0 and v < 1
+
+static func distance_point_to_segment(point: Vector2, segment_start: Vector2, segment_end: Vector2) -> float:
+	var a = segment_end - segment_start
+	var b = point - segment_start
+	var t = b.dot(a) / a.dot(a)
+	if t < 0:
+		t = 0
+	elif t > 1:
+		t = 1
+	var projection = segment_start + a * t
+	return point.distance_to(projection)
