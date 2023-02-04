d1203f0dd20789deb0a671e5eeb9e964ed1b910d
Tomislav Ignjatov <tomislav.ignjatov@intertec.io>
2023-02-04 18:45:18 +0100

    transform roots to spike if they are connected to resource

--- src/Objects/Point.gd
+++ src/Objects/Point.gd
@@ -4,11 +4,21 @@ class_name Point
 var _current_owner = null
 var _connections: Dictionary = {}
 var connection_scene = load("res://Objects/RootPath.tscn")
+var health_points = 1
+
+func spike_transform(point: Point):
+	var spike_texture = preload("res://icon.png")
+	get_node("Sprite").set_texture(spike_texture)
+	health_points = 3
 
 func connect_point(point: Point):
 	var root_path = create_edge(point)
 	_connections[point] = root_path
 	point.get_connections()[self] = root_path
+	if "score" in self:
+		point.spike_transform(point)
+	elif "score" in point:
+		spike_transform(self)
 
 func create_edge(point: Point):
 	var connection = connection_scene.instance()
